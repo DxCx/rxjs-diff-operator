f73010044905a84183df5712c76dfb40a60af203
Hagai Cohen <hagai.co@gmail.com>
2016-09-26 23:12:37 +0300

    tests: add more tests

--- src/fromDiff.spec.ts
+++ src/fromDiff.spec.ts
@@ -46,8 +46,7 @@ describe('fromDiff operator', () => {
                 'value': 1,
             },
             'type': 'init',
-        },
-        {
+        }, {
             'payload': [
                 {
                     'kind': 'E',
@@ -59,8 +58,7 @@ describe('fromDiff operator', () => {
                 },
             ],
             'type': 'update',
-        },
-        {
+        }, {
             'payload': [
                 {
                     'kind': 'E',
@@ -72,8 +70,7 @@ describe('fromDiff operator', () => {
                 },
             ],
             'type': 'update',
-        },
-        {
+        }, {
             'payload': [
                 {
                     'kind': 'E',
@@ -85,8 +82,7 @@ describe('fromDiff operator', () => {
                 },
             ],
             'type': 'update',
-        },
-        {
+        }, {
             'payload': [
                 {
                     'kind': 'E',
@@ -98,8 +94,7 @@ describe('fromDiff operator', () => {
                 },
             ],
             'type': 'update',
-        },
-        {
+        }, {
             'type': 'complete',
         });
 
@@ -107,6 +102,153 @@ describe('fromDiff operator', () => {
             expect(typeof msgs).toBe('object');
             expect(Array.isArray(msgs)).toBe(true);
             expect(msgs).toMatchSnapshot();
+        });
+    });
+
+    it('throws when get error', () => {
+        let obs: Observable<IObservableDiff> = Observable.of<IObservableDiff>({
+            'isObject': true,
+            'payload': {
+                'value': 1,
+            },
+            'type': 'init',
+        }, {
+            'payload': [
+                {
+                    'kind': 'E',
+                    'lhs': 1,
+                    'path': [
+                        'value',
+                    ],
+                    'rhs': 2,
+                },
+            ],
+            'type': 'update',
+        }, {
+            'payload': 'testing errors',
+            'type': 'error',
+        });
+
+        return obs.fromDiff().toPromise().then(msgs => {
+            throw new Error('shouldn\'t get here because of the promise');
+        }, (e: Error) => {
+            expect(() => { throw e; }).toThrow('testing errors');
+        });
+    });
+
+    it('throws when missing init message', () => {
+        let obs: Observable<IObservableDiff> = Observable.of<IObservableDiff>({
+            'payload': [
+                {
+                    'kind': 'E',
+                    'lhs': 1,
+                    'path': [
+                        'value',
+                    ],
+                    'rhs': 2,
+                },
+            ],
+            'type': 'update',
+        }, {
+            'payload': 'testing errors',
+            'type': 'error',
+        });
+
+        return obs.fromDiff().toPromise().then(msgs => {
+            throw new Error('shouldn\'t get here because of the promise');
+        }, (e: Error) => {
+            expect(() => { throw e; }).toThrow('Init message was not emitted');
+        });
+    });
+
+    it('throws when init message repeated', () => {
+        let obs: Observable<IObservableDiff> = Observable.of<IObservableDiff>({
+            'isObject': true,
+            'payload': {
+                'value': 1,
+            },
+            'type': 'init',
+        }, {
+            'payload': [
+                {
+                    'kind': 'E',
+                    'lhs': 1,
+                    'path': [
+                        'value',
+                    ],
+                    'rhs': 2,
+                },
+            ],
+            'type': 'update',
+        }, {
+            'isObject': true,
+            'payload': {
+                'value': 1,
+            },
+            'type': 'init',
+        }, {
+            'payload': 'testing errors',
+            'type': 'error',
+        });
+
+        return obs.fromDiff().toPromise().then(msgs => {
+            throw new Error('shouldn\'t get here because of the promise');
+        }, (e: Error) => {
+            expect(() => { throw e; }).toThrow('Init message emitted while in sequance');
+        });
+    });
+
+    it('throws when no type provided', () => {
+        let obs: Observable<IObservableDiff> = Observable.of<IObservableDiff>({
+            'isObject': true,
+            'payload': {
+                'value': 1,
+            },
+            'type': 'init',
+        }, {
+            'isObject': true,
+            'payload': {
+                'value': 1,
+            },
+            'type': undefined,
+        });
+
+        return obs.fromDiff().toPromise().then(msgs => {
+            throw new Error('shouldn\'t get here because of the promise');
+        }, (e: Error) => {
+            expect(() => { throw e; }).toThrow('unexpected message');
+        });
+    });
+
+    it('throws when no type malformed', () => {
+        let obs: Observable<IObservableDiff> = Observable.of<IObservableDiff>({
+            'isObject': true,
+            'payload': {
+                'value': 1,
+            },
+            'type': 'init',
+        }, {
+            'isObject': true,
+            'payload': {
+                'value': 1,
+            },
+            'type': 'something',
+        });
+
+        return obs.fromDiff().toPromise().then(msgs => {
+            throw new Error('shouldn\'t get here because of the promise');
+        }, (e: Error) => {
+            expect(() => { throw e; }).toThrow('unexpected message');
+        });
+    });
+
+    it('throws when input is malformed', () => {
+        let obs: Observable<any> = Observable.of<any>(123);
+
+        return obs.fromDiff().toPromise().then(msgs => {
+            throw new Error('shouldn\'t get here because of the promise');
+        }, (e: Error) => {
+            expect(() => { throw e; }).toThrow('Init message was not emitted.');
         });
     });
 });
--- src/toDiff.spec.ts
+++ src/toDiff.spec.ts
@@ -31,4 +31,22 @@ describe('toDiff operator', () => {
             expect(msgs).toMatchSnapshot();
         });
     });
+
+    it('emits errors as well', () => {
+        let obs: Observable<{ value: number }> = Observable.of(1, 2, 3, 4, 5)
+        .map((v: number, i: number) => {
+            if ( i === 2 ) {
+                throw new Error('testing errors');
+            }
+
+            return { value: v };
+        });
+
+        return obs.toDiff().bufferCount(6).toPromise().then(msgs => {
+            expect(typeof msgs).toBe('object');
+            expect(msgs[0].type).toBe('init');
+            expect(msgs[2].type).toBe('error');
+            expect(msgs).toMatchSnapshot();
+        });
+    });
 });
