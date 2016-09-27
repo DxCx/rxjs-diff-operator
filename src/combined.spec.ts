e9a8b29e1d7ca0bdd9ccc155e208498c19210d4c
Hagai Cohen <hagai.co@gmail.com>
2016-09-27 17:20:42 +0300

    tests: added combined pipe test

---
+++ src/combined.spec.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import 'jest';
+
+import { Observable } from 'rxjs';
+import './index';
+
+describe('diff operator combined', () => {
+    it('works togather', () => {
+        let obs: Observable<number> = Observable.of(1, 2, 3, 4, 5);
+
+        return obs.toDiff().fromDiff().bufferCount(6).toPromise().then(msgs => {
+            expect(typeof msgs).toBe('object');
+            expect(msgs).toMatchSnapshot();
+        });
+    });
+});
